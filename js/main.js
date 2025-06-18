ee55713dff2efb457940e3bc7c42165f281ae68c
Andrei Jebelean <ajebelean.mmt-b2023@fh-salzburg.ac.at>
2025-06-18 10:57:50 +0200

    added new icons and a new location

--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
 //--- Part 1: Create the map and add base map ---
-// Central marker coordinates
 var centerLat = 47.7990583;
 var centerLng = 13.0499556;
-// Offset latitude slightly south for better visual center
 var offsetLat = centerLat - 0.0015;
 
 var map = L.map('map', {
-	center: [offsetLat, centerLng],
-	zoom: 15
+    center: [offsetLat, centerLng],
+    zoom: 15
 });
 
 var osmap = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}', {
@@ -18,7 +16,7 @@ var osmap = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/
 }).addTo(map);
 
 var baseMaps = {
-	"Open Street Map": osmap
+    "Open Street Map": osmap
 };
 
 //--- Part 2: Add scale bar ---
@@ -29,15 +27,20 @@ map.addEventListener('dblclick', function(e) {
     alert(e.latlng);
 });
 
-//--- Part 4: Define icons ---
-var iconInfo = L.icon({
-	iconUrl: 'css/images/marker-icon.2x.png',
-	iconSize: [25, 25]
+//--- Part 4: Define custom icons ---
+var iconBuilding = L.icon({
+    iconUrl: 'css/images/building-icon.png',
+    iconSize: [40, 40]
 });
 
-var iconHouse = L.icon({
-	iconUrl: 'css/images/house.png',
-	iconSize: [25, 25]
+var iconStatue = L.icon({
+    iconUrl: 'css/images/statue-icon.png',
+    iconSize: [40, 40]
+});
+
+var iconInfo = L.icon({
+    iconUrl: 'css/images/info-icon.png',
+    iconSize: [40, 40]
 });
 
 //--- Part 5: Prepare layers containers ---
@@ -47,71 +50,78 @@ var touristInfoLayer = L.layerGroup();
 
 //--- Part 6: Load GeoJSON and separate features into layers ---
 fetch('data/mozart.geojson')
-  .then(response => response.json())
-  .then(data => {
-    // Separate GeoJSON features by tourism type
-    data.features.forEach(feature => {
-      var tourismType = feature.properties.tourism || "";
-      var latlng = [feature.geometry.coordinates[1], feature.geometry.coordinates[0]];
+    .then(response => response.json())
+    .then(data => {
+        data.features.forEach(feature => {
+            var tourismType = feature.properties.tourism || "";
+            var latlng = [feature.geometry.coordinates[1], feature.geometry.coordinates[0]];
+            var nameLower = (feature.properties.name || "").toLowerCase();
+            var id = feature.id || feature.properties["@id"] || "";
 
-      // Decide icon based on name or tourism type
-      var nameLower = (feature.properties.name || "").toLowerCase();
-      var isInfoPoint = nameLower.includes("info");
-      var icon = isInfoPoint ? iconInfo : iconHouse;
+            // --- Icon logic ---
+            let icon;
+            if (id.includes("mozarts_birthplace") || nameLower.includes("mozart residence")) {
+                icon = iconBuilding;
+            } else if (tourismType === "information" || nameLower.includes("info")) {
+                icon = iconInfo;
+            } else if (tourismType === "artwork") {
+                icon = iconStatue;
+            } else {
+                icon = iconStatue; // fallback icon
+            }
 
-      // Create marker with popup
-      var marker = L.marker(latlng, { icon: icon, title: feature.properties.name || "Mozart Attraction" });
+            // Create marker with popup
+            var marker = L.marker(latlng, { icon: icon, title: feature.properties.name || "Mozart Attraction" });
 
-      // Build popup content
-      var name = feature.properties.name || "Unknown Location";
-      var description = feature.properties.description || "";
-      var url = feature.properties.website || "#";
-      var imageUrl = feature.properties.image || null;
+            // Build popup content
+            var name = feature.properties.name || "Unknown Location";
+            var description = feature.properties.description || "";
+            var url = feature.properties.website || "#";
+            var imageUrl = feature.properties.image || null;
 
-      var popupContent = `<b>${name}</b><br>`;
-      if (description) popupContent += `${description}<br>`;
-      popupContent += `<a href="${url}" target="_blank" rel="noopener noreferrer">Read more</a>`;
-      if (imageUrl) {
-        popupContent += `<br><img src="${imageUrl}" alt="${name}" style="max-width:200px; margin-top:5px;">`;
-      }
+            var popupContent = `<b>${name}</b><br>`;
+            if (description) popupContent += `${description}<br>`;
+            popupContent += `<a href="${url}" target="_blank" rel="noopener noreferrer">Read more</a>`;
+            if (imageUrl) {
+                popupContent += `<br><img src="${imageUrl}" alt="${name}" style="max-width:200px; margin-top:5px;">`;
+            }
 
-      marker.bindPopup(popupContent);
+            marker.bindPopup(popupContent);
 
-      // On click, fly to location with offset and open popup
-      marker.on('click', function() {
-        var pos = marker.getLatLng();
-        // Offset the lat slightly south
-        map.flyTo([pos.lat - 0.0015, pos.lng], 18, { duration: 1.5 });
-        setTimeout(() => {
-          marker.openPopup();
-        }, 1600);
-      });
+            // On click, fly to location with offset and open popup
+            marker.on('click', function() {
+                var pos = marker.getLatLng();
+                map.flyTo([pos.lat - 0.0015, pos.lng], 18, { duration: 1.5 });
+                setTimeout(() => {
+                    marker.openPopup();
+                }, 1600);
+            });
 
-      // Add marker to the correct layer based on tourism type
-      if (tourismType === "artwork") {
-        artLayer.addLayer(marker);
-      } else if (tourismType === "museum") {
-        residenceLayer.addLayer(marker);
-      } else if (tourismType === "information") {
-        touristInfoLayer.addLayer(marker);
-      }
-    });
+            // Add marker to appropriate layer
+            if (tourismType === "artwork") {
+                artLayer.addLayer(marker);
+            } else if (tourismType === "museum") {
+                residenceLayer.addLayer(marker);
+            } else if (tourismType === "information") {
+                touristInfoLayer.addLayer(marker);
+            }
+        });
 
-    // Add all layers to map initially (or you can choose to add only some)
-    artLayer.addTo(map);
-    residenceLayer.addTo(map);
-    touristInfoLayer.addTo(map);
+        // Add all layers to map
+        artLayer.addTo(map);
+        residenceLayer.addTo(map);
+        touristInfoLayer.addTo(map);
 
-    // Add overlays for layer control
-    features["Art"] = artLayer;
-    features["Residences"] = residenceLayer;
-    features["Tourist Info"] = touristInfoLayer;
+        // Add overlays for layer control
+        features["Art"] = artLayer;
+        features["Residences"] = residenceLayer;
+        features["Tourist Info"] = touristInfoLayer;
 
-    layerControl.addOverlay(artLayer, "Art");
-    layerControl.addOverlay(residenceLayer, "Residences");
-    layerControl.addOverlay(touristInfoLayer, "Tourist Info");
-  })
-  .catch(err => console.error('Error loading mozart.geojson:', err));
+        layerControl.addOverlay(artLayer, "Art");
+        layerControl.addOverlay(residenceLayer, "Residences");
+        layerControl.addOverlay(touristInfoLayer, "Tourist Info");
+    })
+    .catch(err => console.error('Error loading mozart.geojson:', err));
 
 //--- Part 7: Layer control ---
 var features = {};
@@ -119,6 +129,6 @@ var layerControl = L.control.layers(baseMaps, features, {position: 'topleft'}).a
 
 //--- Part 8: Central marker ---
 L.marker([centerLat, centerLng])
-  .addTo(map)
-  .bindPopup("Mozart Tour Starts Here!")
-  .openPopup();
+    .addTo(map)
+    .bindPopup("Mozart Tour Starts Here!")
+    .openPopup();
