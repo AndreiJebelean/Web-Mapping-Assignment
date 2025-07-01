443c7cfe76d123de35280c2b1947064e10867da5
Andrei Jebelean <ajebelean.mmt-b2023@fh-salzburg.ac.at>
2025-07-01 20:12:30 +0200

    changed the map

--- js/main.js
+++ js/main.js
@@ -8,12 +8,11 @@ var map = L.map('map', {
   zoom: 15
 });
 
-var osmap = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}', {
-  minZoom: 0,
-  maxZoom: 20,
-  attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  ext: 'png'
+var osmap = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
+  attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; OpenMapTiles &copy; OpenStreetMap contributors',
+  maxZoom: 20
 }).addTo(map);
+
 
 var baseMaps = {
   "Open Street Map": osmap
