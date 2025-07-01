155e62b718570dcca45e0cbc613cad8c57dc4025
Andrei Jebelean <ajebelean.mmt-b2023@fh-salzburg.ac.at>
2025-07-01 20:15:56 +0200

    map adjustments

--- js/main.js
+++ js/main.js
@@ -8,9 +8,10 @@ var map = L.map('map', {
   zoom: 15
 });
 
-var osmap = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
-  attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; OpenMapTiles &copy; OpenStreetMap contributors',
-  maxZoom: 20
+var osmap = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+  attribution: '&copy; <a href="https://carto.com/">CARTO</a> © OpenStreetMap contributors',
+  subdomains: 'abcd',
+  maxZoom: 19
 }).addTo(map);
 
 
